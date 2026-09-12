0b445eda2f596ad45d3c6e94db2740fe63c3872b
Leandro Martinelli <leandro@leandromartinelli.com.br>
2026-09-12 16:37:11 -0300

    feat(terraform): add AKS cluster
    Provision AKS with Azure CNI, Azure RBAC, managed identity and a dedicated system node pool.

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,3 +30,38 @@ resource "azurerm_container_registry" "acr" {
 
   tags = var.tags
 }
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.aks_lab.location
+  resource_group_name = azurerm_resource_group.aks_lab.name
+  dns_prefix          = var.aks_dns_prefix
+  kubernetes_version  = var.aks_kubernetes_version
+
+  default_node_pool {
+    name           = "system"
+    node_count     = var.aks_system_node_pool_count
+    vm_size        = var.aks_system_node_pool_vm_size
+    vnet_subnet_id = azurerm_subnet.aks.id
+
+    only_critical_addons_enabled = true
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  role_based_access_control_enabled = true
+
+  azure_active_directory_role_based_access_control {
+    azure_rbac_enabled = true
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    network_policy    = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  tags = var.tags
+}
