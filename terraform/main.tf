47e98d2b7349f17ba465852a87bc2145e6d4c0a0
Leandro Martinelli <leandro@leandromartinelli.com.br>
2026-09-12 16:32:53 -0300

    feat(terraform): add Azure Container Registry
    Provision Azure Container Registry for storing AKS container images.

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,13 @@ resource "azurerm_subnet" "aks" {
   virtual_network_name = azurerm_virtual_network.aks_lab.name
   address_prefixes     = var.aks_subnet_address_prefixes
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.aks_lab.name
+  location            = azurerm_resource_group.aks_lab.location
+  sku                 = var.acr_sku
+  admin_enabled       = false
+
+  tags = var.tags
+}
